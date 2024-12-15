dbcfda0aeb43e7cf559f3d1780ee2da9b4af727e
Redha Chenouf <r.chenouf03@gmail.com>
2024-12-15 21:31:31 +0100

    Ajout du portfolio complet

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// Scroll animations
 document.querySelectorAll('section').forEach(section => {
     section.style.opacity = 0;
-    section.style.transition = 'opacity 1s';
+    section.style.transition = 'opacity 1s ease-out';
 });
 
 window.addEventListener('scroll', () => {
     document.querySelectorAll('section').forEach(section => {
         const rect = section.getBoundingClientRect();
-        if (rect.top <= window.innerHeight / 1.3) {
+        if (rect.top <= window.innerHeight / 1.2) {
             section.style.opacity = 1;
         }
     });
